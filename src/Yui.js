cef28ec85afd6b916e1fcd5d4f423447dae7ea4a
InkoHX <inkohx@gmail.com>
2019-02-27 21:43:31 +0900

    Update presence

--- src/Yui.js
+++ src/Yui.js
@@ -4,6 +4,6 @@ const {token} = require('../config')
 new Client({
   prefix: 'yui!',
   regexPrefix: /^yuigahama(@|!)/i,
-  presence: { activity: { name: 'Yuigahama!help', type: 'LISTENING' } },
+  presence: { activity: { name: 'Yuigahama!help', type: 'PLAYING' } },
   language: 'ja-JP'
 }).login(token)
