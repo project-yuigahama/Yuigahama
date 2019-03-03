4fd639aa8a7f153e256758baab5f72f67ded136a
InkoHX <inkohx@gmail.com>
2019-03-03 16:39:29 +0900

    Update guideinfo

--- src/commands/Guide/guideinfo.js
+++ src/commands/Guide/guideinfo.js
@@ -24,17 +24,28 @@ module.exports = class extends Command {
       `Offline: ${message.guild.members.filter(member =>  member.presence.status === 'offline').size}`,
       `Bot: ${message.guild.members.filter(member => member.user.bot === true).size}`
     ].join('\n')
+    const Channels = [
+      `Total: ${message.guild.channels.size}`,
+      `Category: ${message.guild.channels.filter(channel => channel.type === 'category').size}`,
+      `Text: ${message.guild.channels.filter(channel => channel.type === 'text').size}`,
+      `Voice: ${message.guild.channels.filter(channel => channel.type === 'voice').size}`
+    ].join('\n')
+    const Emojis = [
+      `Total: ${message.guild.emojis.size}`,
+      `Emoji: ${message.guild.emojis.filter(emoji => emoji.animated === false).size}`,
+      `Animation: ${message.guild.emojis.filter(emoji => emoji.animated === true).size}`
+    ].join('\n')
     return message.sendEmbed(new MessageEmbed()
       .setTitle(message.guild.name)
       .setThumbnail(message.guild.iconURL({size: 512}))
       .setTimestamp(message.guild.createdAt)
       .setFooter('Created At')
       .addField('Guide ID', message.guild.id, true)
-      .addField('Owner', message.guild.owner.displayName, true)
+      .addField('Owner', message.guild.owner.user.tag, true)
       .addField('Members', memberCount, true)
-      .addField('Channels', message.guild.channels.size, true)
-      .addField('Emojis', message.guild.emojis.size, true)
-      .addField('Roles', message.guild.roles.size, true)
+      .addField('Channels', Channels, true)
+      .addField('Emojis', Emojis, true)
+      .addField('Roles', message.guild.roles.map(role => role.name).join(', '), true)
       .addField('Region', message.guild.region.toUpperCase(), true)
     )
   }
