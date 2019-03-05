951375016a5cbc55f07bf5258cee8778d40c5f84
InkoHX <inkohx@gmail.com>
2019-03-05 22:19:41 +0900

    Change

--- src/commands/User/avatar.js
+++ src/commands/User/avatar.js
@@ -5,7 +5,6 @@ module.exports = class extends Command {
   constructor (...args) {
     super(...args, {
       enabled: true,
-      runIn: ['text'],
       requiredPermissions: [],
       description: '',
       extendedHelp: 'No extended help available.',
