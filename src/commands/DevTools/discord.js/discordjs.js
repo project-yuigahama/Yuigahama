c9c26ca5c812451e13364a02aa6b23d0de4b7d18
InkoHX <inkohx@gmail.com>
2019-03-07 19:09:59 +0900

    Add discordjs command

---
+++ src/commands/DevTools/discord.js/discordjs.js
@@ -0,0 +1,67 @@
+const { Command, KlasaMessage } = require('klasa')
+const request = require('request')
+
+/**
+ * @extends Command
+ */
+class DiscordJS extends Command {
+  constructor (...args) {
+    super(...args, {
+      aliases: ['djs', 'djsdocs'],
+      requiredPermissions: ['EMBED_LINKS'],
+      description: 'Discord.jsのドキュメントを検索します。',
+      usage: '<commando|rpc|main:default> <query:string> [branch:string]',
+      usageDelim: ' ',
+      subcommands: true
+    })
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   * @param {string[]} query
+   */
+  async main (message, [query, branch = 'stable']) {
+    await this.search(message, query, 'main', branch)
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   * @param {string[]} query
+   */
+  async commando (message, [query]) {
+    await this.search(message, query, 'commando')
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   * @param {string[]} query
+   */
+  async rpc (message, [query]) {
+    await this.search(message, query, 'rpc')
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   * @param {string} query
+   * @param {string} type
+   * @param {string} branch
+   *
+   * @private
+   * @static
+   */
+  async search (message, query, type = 'main', branch = 'master') {
+    await request(`https://djsdocs.sorta.moe/${type}/${branch}/embed`, {
+      qs: {
+        q: query
+      },
+      method: 'GET',
+      json: true
+    }, (e, r, b) => {
+      if (b.status) return message.sendMessage(b.message)
+      if (!b) return message.sendMessage(`[${type}:${branch}] Information matching \`${query}\` could not be found.`)
+      return message.sendEmbed(b)
+    })
+  }
+}
+
+module.exports = DiscordJS
