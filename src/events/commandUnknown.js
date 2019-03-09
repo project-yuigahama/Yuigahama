a01cfcc66f50ecdb78fff5a388efd64c75450b92
InkoHX <inkohx@gmail.com>
2019-03-09 20:00:36 +0900

    ✨ : Add CommandUnknown message

---
+++ src/events/commandUnknown.js
@@ -0,0 +1,15 @@
+const { Event, KlasaMessage } = require('klasa')
+
+class CommandUnknown extends Event {
+  /**
+   * @param {KlasaMessage} message
+   * @param {String} commandText
+   * @param {RegExp} prefix
+   * @param {Int} prefixLength
+   */
+  run (message, commandText, prefix, prefixLength) {
+    message.sendMessage(`\`${commandText}\` は存在しないコマンドです。誤字、脱字が無いか \`yuigahama!help\` で確認して下さい。 https://tenor.com/view/anime-panic-what-to-do-omg-worried-gif-13451210`)
+  }
+}
+
+module.exports = CommandUnknown
