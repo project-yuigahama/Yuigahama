77ad7f6ec3b16639c09dd90cb2d47b20a34b0872
InkoHX <inkohx@gmail.com>
2019-03-10 12:40:36 +0900

    ✨ : Update process name

--- src/Yui.js
+++ src/Yui.js
@@ -1,14 +1,19 @@
 const { Client } = require('klasa')
 const { token } = require('../config')
-
-new Client({
+const client = new Client({
   prefix: 'yui!',
   regexPrefix: /^yuigahama(@|!)/i,
   presence: { activity: { name: 'Yuigahama!help', type: 'PLAYING' } },
   language: 'ja-JP',
   commandLogging: true,
   commandEditing: true
-}).login(token)
+})
+
+client.login(token)
+
+setInterval(async () => {
+  process.title = `Memory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}/${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)}MB - YuigahamaBot`
+}, 1000)
 
 module.exports = {
   Utils: require('./lib/util/utils')
