1f348b57b54e5a4cd4d1ad20609a67b53102dcb6
InkoHX <inkohx@gmail.com>
2019-03-10 12:41:32 +0900

    ✨ : Add channelinfo command

---
+++ src/commands/Guide/Information/channelinfo.js
@@ -0,0 +1,44 @@
+const { Command, KlasaMessage } = require('klasa')
+const { TextChannel, VoiceChannel, DMChannel, MessageEmbed } = require('discord.js')
+
+module.exports = class extends Command {
+  constructor (...args) {
+    super(...args, {
+      runIn: ['text'],
+      requiredPermissions: [],
+      aliases: [],
+      description: '',
+      extendedHelp: 'No extended help available.',
+      usage: '<channel:channel>'
+    })
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   * @param {TextChannel[]|VoiceChannel[]} channel
+   */
+  async run (message, [channel]) {
+    if (channel instanceof TextChannel) {
+      return message.sendEmbed(new MessageEmbed()
+        .setTitle('ChannelInfo')
+        .setDescription(channel.topic || 'None')
+        .addField('Name', channel.name, true)
+        .addField('ID', channel.id, true)
+        .addField('Type', 'Text Channel', true)
+        .addField('Typing Count', channel.typingCount, true)
+        .addField('NSFW', channel.nsfw ? 'Yes' : 'No', true)
+      )
+    }
+    if (channel instanceof VoiceChannel) {
+      return message.sendEmbed(new MessageEmbed()
+        .setTitle('ChannelInfo')
+        .addField('Name', channel.name, true)
+        .addField('ID', channel.id, true)
+        .addField('Type', 'Voice Channel', true)
+        .addField('Members', channel.members.size, true)
+        .addField('Limit', channel.userLimit ? channel.userLimit : 'Infinity', true)
+      )
+    }
+    return message.sendMessage('そのチャンネルはサポートしていません。<:BlobFearSweat:528112258794913803>')
+  }
+}
