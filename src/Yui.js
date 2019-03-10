511c9886416dc3722646a0b7fe9e3956443cb113
InkoHX <inkohx@gmail.com>
2019-03-10 15:24:04 +0900

    ✨ : Update new process title

--- src/Yui.js
+++ src/Yui.js
@@ -1,5 +1,6 @@
 const { Client } = require('klasa')
 const { token } = require('../config')
+const { version } = require('../package.json')
 const client = new Client({
   prefix: 'yui!',
   regexPrefix: /^yuigahama(@|!)/i,
@@ -11,8 +12,14 @@ const client = new Client({
 
 client.login(token || process.env.DISCORD_TOKEN)
 
-setInterval(async () => {
-  process.title = `Memory: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}/${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)}MB - YuigahamaBot`
+setInterval(() => {
+  const heapUsed = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)
+  const heapTotal = (process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)
+  const onlineUser = client.users.filter(user => user.presence.status === 'online').size
+  const totalUser = client.users.size
+  const guilds = client.guilds.size
+  const ping = Math.round(client.ws.ping)
+  process.title = `YuigahamaBot v${version} - Memory: ${heapUsed}/${heapTotal} MB | Heartbeat: ${ping}ms | Users: ${onlineUser}/${totalUser} | ${guilds} guilds`
 }, 1000)
 
 module.exports = {
