bcdb364ae5b2ab8e5023ee4246a4f2e5939a81a1
InkoHX <inkohx@gmail.com>
2019-03-19 22:26:47 +0900

    fixes

--- src/commands/Guide/Manager/ban.js
+++ src/commands/Guide/Manager/ban.js
@@ -20,14 +20,14 @@ class Ban extends Command {
    * @param {[KlasaUser, number, string]} usage
    */
   async run (message, [user, days = 0, reason = 'Not specified']) {
-    const member = await message.guild.members.fetch(user).catch(() => KlasaUser)
-    if (message.author.id === member.id) return message.sendMessage('自分自身をBANする事は出来ません。')
-    if (this.client.user.id === member.id) return message.sendMessage('私をBANする場合このコマンドは実行出来ません。')
+    const member = await message.guild.members.fetch(user).catch(() => null)
+    if (message.author.id === user.id) return message.sendMessage('自分自身をBANする事は出来ません。')
+    if (this.client.user.id === user.id) return message.sendMessage('私をBANする場合このコマンドは実行出来ません。')
     if (member instanceof GuildMember) {
       if (member.roles.highest.position >= message.member.roles.highest.position || !member.bannable) return message.sendMessage('このユーザーはBAN出来ません。理由: 自分より上の権限を持ったユーザーかこのボットの権限ではこのユーザーをBAN出来ない為')
     }
     await message.guild.members.ban(user, { days: days, reason: reason })
-    return message.sendMessage(`BANしました。<:SataniaThumbsUp:551707177249800204>`)
+    return message.sendMessage(`${user.tag}をBANしました。<:SataniaThumbsUp:551707177249800204>`)
   }
 }
 
