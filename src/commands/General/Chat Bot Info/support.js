ead1735de3382533c238e6b6fe814e6b9b3b9ac8
InkoHX <inkohx@gmail.com>
2019-03-25 00:03:29 +0900

    Add support command

---
+++ src/commands/General/Chat Bot Info/support.js
@@ -0,0 +1,19 @@
+const { Command, KlasaMessage } = require('klasa')
+
+class Support extends Command {
+  constructor (...args) {
+    super(...args, {
+      description: 'このボットの公式ギルドの招待URLを送信します。',
+      extendedHelp: 'No extended help available.'
+    })
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   */
+  async run (message) {
+    return message.sendMessage('None')
+  }
+}
+
+module.exports = Support
