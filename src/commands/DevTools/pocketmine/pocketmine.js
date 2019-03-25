6c625af5159b33ab7d0a3d09c78354510954fd62
InkoHX <inkohx@gmail.com>
2019-03-25 16:36:20 +0900

    Update pocketmine command

--- src/commands/DevTools/pocketmine/pocketmine.js
+++ src/commands/DevTools/pocketmine/pocketmine.js
@@ -15,20 +15,20 @@ module.exports = class extends Command {
    * @param {KlasaMessage} message
    */
   async run (message) {
-    const body = fetch('https://update.pmmp.io/api').then(res => res.json()).catch(() => null)
+    const body = await fetch('https://update.pmmp.io/api').then(res => res.json()).catch(() => null)
     if (!body) throw new Error()
     return message.sendEmbed(new MessageEmbed()
       .setTitle('PocketMine-MP (Stable)')
       .setURL(body['details_url'])
       .setThumbnail('https://cdn.pocketmine.net/favicon.png')
-      .addField('> PHP', body['php_version'])
-      .addField('> API', body['base_version'])
-      .addField('> Build Number', body['build_number'])
-      .addField('> Minecraft Version', body['mcpe_version'])
-      .addField('> Dev', body['is_dev'] ? 'Yes' : 'No')
-      .addField('> Commit Hash', body['git_commit'])
+      .setColor('RANDOM')
+      .addField('> PHP', body['php_version'], true)
+      .addField('> API', body['base_version'], true)
+      .addField('> Build Number', body['build_number'], true)
+      .addField('> Minecraft Version', body['mcpe_version'], true)
+      .addField('> Dev', body['is_dev'] ? 'Yes' : 'No', true)
+      .addField('> Commit Hash', body['git_commit'], true)
       .setFooter('pmmp/PocketMine-MP', 'https://avatars1.githubusercontent.com/u/22548559?s=200&v=4')
-      .setTimestamp(body['date'])
     )
   }
 }
