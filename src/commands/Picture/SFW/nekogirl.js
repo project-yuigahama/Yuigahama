973007d1aaadff36b9f8ffbb543dc191f1e04e4f
InkoHX <inkohx@gmail.com>
2019-03-25 20:46:16 +0900

    neko > nekogirl

--- src/commands/Picture/SFW/nekogirl.js
+++ src/commands/Picture/SFW/nekogirl.js
@@ -2,7 +2,12 @@ const { Command, KlasaMessage } = require('klasa')
 const { MessageAttachment } = require('discord.js')
 const { Nekoslife } = require('../../../Yui')
 
-module.exports = class extends Command {
+const api = new Nekoslife()
+
+/**
+ * @extends Command
+ */
+class NekoGirl extends Command {
   constructor (...args) {
     super(...args, {
       description: 'かわいい猫耳女の子の画像を送信します。',
@@ -14,8 +19,9 @@ module.exports = class extends Command {
    * @param {KlasaMessage} message
    */
   async run (message) {
-    const api = new Nekoslife()
     if ('gif' in message.flags) return message.send(new MessageAttachment((await api.request(Nekoslife.END_POINTS_V3.neko_gif, true))['data']['response']['url']))
     else return message.send(new MessageAttachment((await api.request(Nekoslife.END_POINTS_V3.neko, true))['data']['response']['url']))
   }
 }
+
+module.exports = NekoGirl
