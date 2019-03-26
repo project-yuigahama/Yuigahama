774a15979ed7c6c0a9dcc7cbcacec0f9bcae1959
InkoHX <inkohx@gmail.com>
2019-03-26 18:23:59 +0900

    lewd-fox コマンドを追加

---
+++ src/commands/Picture/NSFW/lewd-fox.js
@@ -0,0 +1,27 @@
+const { Command, KlasaMessage } = require('klasa')
+const { MessageAttachment } = require('discord.js')
+const { Nekoslife } = require('../../../Yui')
+
+const api = new Nekoslife()
+
+/**
+ * @extends Command
+ */
+class LewdFox extends Command {
+  constructor (...args) {
+    super(...args, {
+      nsfw: true,
+      description: 'いやらしい狐耳の女の子画像を送信します。',
+      extendedHelp: 'No extended help available.'
+    })
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   */
+  async run (message) {
+    return message.send(new MessageAttachment((await api.request(Nekoslife.END_POINTS_V3.nsfw.fox, true))['data']['response']['url']))
+  }
+}
+
+module.exports = LewdFox
