76a62dd6bb3b727f26f1607068588fead56b5b9f
InkoHX <inkohx@gmail.com>
2019-03-26 18:24:14 +0900

    lewd-neko コマンドの追加

---
+++ src/commands/Picture/NSFW/lewd-neko.js
@@ -0,0 +1,28 @@
+const { Command, KlasaMessage } = require('klasa')
+const { MessageAttachment } = require('discord.js')
+const { Nekoslife } = require('../../../Yui')
+
+const api = new Nekoslife()
+
+/**
+ * @extends Command
+ */
+class LewdNeko extends Command {
+  constructor (...args) {
+    super(...args, {
+      nsfw: true,
+      description: 'いやらしい猫耳の女の子画像を送信します。',
+      extendedHelp: ['--gif を付けるとGIF画像が送信されます。']
+    })
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   */
+  async run (message) {
+    if ('gif' in message.flags) return message.send(new MessageAttachment((await api.request(Nekoslife.END_POINTS_V3.nsfw.neko_gif, true))['data']['response']['url']))
+    else return message.send(new MessageAttachment((await api.request(Nekoslife.END_POINTS_V3.nsfw.neko, true))['data']['response']['url']))
+  }
+}
+
+module.exports = LewdNeko
