eba0e844b8accbd3ef0f2cabd70ca0fceee186c1
InkoHX <inkohx@gmail.com>
2019-03-26 18:23:43 +0900

    hentai コマンドを追加

---
+++ src/commands/Picture/NSFW/hentai.js
@@ -0,0 +1,28 @@
+const { Command, KlasaMessage } = require('klasa')
+const { MessageAttachment } = require('discord.js')
+const { Nekoslife } = require('../../../Yui')
+
+const api = new Nekoslife()
+
+/**
+ * @extends Command
+ */
+class Hentai extends Command {
+  constructor (...args) {
+    super(...args, {
+      nsfw: true,
+      description: 'いやらしい画像を送信します。',
+      extendedHelp: ['--gif を付けるとGIF画像が送信されます。']
+    })
+  }
+
+  /**
+   * @param {KlasaMessage} message
+   */
+  async run (message) {
+    if ('gif' in message.flags) return message.send(new MessageAttachment((await api.request(Nekoslife.END_POINTS_V3.nsfw.hentai_gif, true))['data']['response']['url']))
+    else return message.send(new MessageAttachment((await api.request(Nekoslife.END_POINTS_V3.nsfw.hentai, true))['data']['response']['url']))
+  }
+}
+
+module.exports = Hentai
