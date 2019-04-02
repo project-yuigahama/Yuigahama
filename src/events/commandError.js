8e0755168e4de797b44bbb074a6b07a0cbc74d5f
InkoHX <inkohx@gmail.com>
2019-04-02 14:36:05 +0900

    error gif

--- src/events/commandError.js
+++ src/events/commandError.js
@@ -10,7 +10,7 @@ class commandError extends Event {
    */
   run (message, command, params, error) {
     if (error instanceof Error) this.client.emit('wtf', `[COMMAND] ${command.path}\n${error.stack || error}`)
-    if (error.message) message.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err))
+    if (error.message) message.sendCode('JSON', error.message).catch(err => this.client.emit('wtf', err)).finally(() => message.sendMessage('https://tenor.com/view/bfb-bfdi-tengolf-tennis-ball-golf-ball-gif-13631169'))
     else message.sendMessage(error).catch(err => this.client.emit('wtf', err))
     if ('ERROR_REPORT_CHANNEL_ID' in process.env && error instanceof Error) {
       if (this.client.ready) this.client.channels.get(process.env.ERROR_REPORT_CHANNEL_ID).sendCode('asciidoc', makeErrorReport(error, this.client)).catch(() => this.client.console.error('channel does not exist.'))
