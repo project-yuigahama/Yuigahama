da0fec755f450b8abc58bcc351eec08bfd9a35ee
InkoHX <inkohx@gmail.com>
2019-04-02 14:35:43 +0900

    put error message

--- src/events/uncaughtException.js
+++ src/events/uncaughtException.js
@@ -7,8 +7,12 @@ class uncaughtException extends Event {
     if (this.client.options.production) this.unload()
   }
 
+  /**
+   * @param {Error} error
+   */
   run (error) {
     if (!(error instanceof Error)) process.exit(1)
+    this.client.emit('error', error.stack || error)
     if ('CRASH_REPORT_CHANNEL_ID' in process.env) {
       if (this.client.ready) {
         this.client.channels.get(process.env.CRASH_REPORT_CHANNEL_ID).sendCode('asciidoc', makeCrashReport(error, this.client))
