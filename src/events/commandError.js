f499fe79a4b0af4a0d5b85cd055b7d7cd78a8046
InkoHX <inkohx@gmail.com>
2019-04-02 15:35:50 +0900

    update

--- src/events/commandError.js
+++ src/events/commandError.js
@@ -12,7 +12,10 @@ class commandError extends Event {
     if (error instanceof Error) this.client.emit('wtf', `[COMMAND] ${command.path}\n${error.stack || error}`)
     message.sendMessage('エラーが発生しました。エラーレポートを作成し送信します。\nhttps://tenor.com/view/anime-stressed-oh-no-slapping-desk-gif-5081298')
     if ('ERROR_REPORT_CHANNEL_ID' in process.env) {
-      if (this.client.ready) this.client.channels.get(process.env.ERROR_REPORT_CHANNEL_ID).sendCode('asciidoc', makeErrorReport(error, this.client))
+      if (this.client.ready) {
+        this.client.channels.get(process.env.ERROR_REPORT_CHANNEL_ID).sendCode('asciidoc', makeErrorReport(error, this.client))
+        message.reply('エラーを報告しました。')
+      }
     } else {
       message.sendCode('asciidoc', makeErrorReport(error, this.client))
       message.reply('エラーレポートチャンネルが指定されていないのでこのチャンネルにレポートを送信しました。コンソールにエラーは送信しましたが解決が遅くなるかもしれません...')
