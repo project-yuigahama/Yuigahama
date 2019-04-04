0c32d992c49777b23529c1d9d890fa089387c74f
InkoHX <inkohx@gmail.com>
2019-04-04 11:46:32 +0900

    fixes

--- src/events/uncaughtException.js
+++ src/events/uncaughtException.js
@@ -15,11 +15,7 @@ class uncaughtException extends Event {
     this.client.emit('error', error.stack || error)
     if ('CRASH_REPORT_CHANNEL_ID' in process.env) {
       if (this.client.ready) {
-        this.client.channels.get(process.env.CRASH_REPORT_CHANNEL_ID).sendCode('asciidoc', makeCrashReport(error, this.client))
-          .catch(() => {
-            this.client.console.error('channel does not exist.')
-            process.exit(1)
-          }).finally(() => process.exit(1))
+        this.client.channels.get(process.env.CRASH_REPORT_CHANNEL_ID).sendCode('asciidoc', makeCrashReport(error, this.client)).finally(() => process.exit(1))
       } else process.exit(1)
     }
   }
