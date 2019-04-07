2c602aac6643ced558582907c417ae937bc81e7c
InkoHX <inkohx@gmail.com>
2019-04-07 18:28:41 +0900

    lol

--- src/events/commandUnknown.js
+++ src/events/commandUnknown.js
@@ -5,7 +5,7 @@ class CommandUnknown extends Event {
    * @param {KlasaMessage} message
    * @param {string} commandText
    * @param {RegExp} prefix
-   * @param {int} prefixLength
+   * @param {number} prefixLength
    */
   run (message, commandText, prefix, prefixLength) {
     message.sendMessage(`\`${commandText}\` は存在しないコマンドです。誤字、脱字が無いか \`yuigahama!help\` で確認して下さい。\nhttps://tenor.com/view/do-not-cry-anime-gif-7999979`)
