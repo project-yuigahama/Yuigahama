10f4ad3010b9702d5bddc84304f61f0b983172b8
InkoHX <inkohx@gmail.com>
2019-04-08 18:43:35 +0900

    fixes

--- src/events/unhandledRejection.js
+++ src/events/unhandledRejection.js
@@ -7,11 +7,18 @@ class unhandledRejection extends Event {
     if (this.client.options.production) this.unload()
   }
 
+  /**
+   * @param {Error} error
+   */
   run (error) {
-    if (!error) return
+    if (!error || error.name === 'DiscordAPIError' || (error.message || '').includes('ENOTFOUND')) return
     this.client.emit('error', `Uncaught Promise Error: \n${error.stack || error}`)
     if ('ERROR_REPORT_CHANNEL_ID' in process.env) {
-      if (this.client.ready) this.client.channels.get(process.env.ERROR_REPORT_CHANNEL_ID).sendCode('asciidoc', makeErrorReport(error, this.client)).catch(() => this.client.console.error('channel does not exist.'))
+      if (this.client.ready) {
+        this.client.channels.get(process.env.ERROR_REPORT_CHANNEL_ID).sendCode('asciidoc', makeErrorReport(error, this.client))
+          .then(() => this.client.console.log('An error report has been sent.'))
+          .catch(() => this.client.console.warn('Failed to send error report.'))
+      }
     }
   }
 }
