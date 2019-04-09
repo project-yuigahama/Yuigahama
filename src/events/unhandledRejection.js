b84e06d49476cbb51da626405a3b4126316e41c7
InkoHX <inkohx@gmail.com>
2019-04-09 23:34:01 +0900

    error

--- src/events/unhandledRejection.js
+++ src/events/unhandledRejection.js
@@ -14,7 +14,7 @@ class unhandledRejection extends Event {
    * @param {Error} error
    */
   run (error) {
-    if (!error || error.name === 'DiscordAPIError' || (error.message || '').includes('ENOTFOUND')) return
+    if (!(error instanceof Error) || error.name === 'DiscordAPIError' || (error.message || '').includes('ENOTFOUND')) return
     this.client.emit('error', `Uncaught Promise Error: \n${error.stack || error}`)
     if ('ERROR_REPORT_CHANNEL_ID' in process.env) {
       if (this.client.ready) {
