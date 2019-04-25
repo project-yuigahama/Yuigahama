a7e9c91049015122c290c83771922e14991e6237
InkoHX <inkohx@gmail.com>
2019-04-25 14:51:21 +0900

    Argument: file

---
+++ src/arguments/attachment.js
@@ -0,0 +1,24 @@
+const { Argument, KlasaMessage, Possible } = require('klasa')
+const { MessageAttachment } = require('discord.js')
+
+class File extends Argument {
+  constructor (...args) {
+    super(...args, {
+      aliases: ['file']
+    })
+  }
+  /**
+   * @param {string} _args
+   * @param {Possible} possible
+   * @param {KlasaMessage} message
+   *
+   * @returns {MessageAttachment}
+   */
+  run (_args, possible, message) {
+    // eslint-disable-next-line no-throw-literal
+    if (!(message.attachments.first() instanceof MessageAttachment)) throw `${possible.name} はファイルである必要があります。`
+    return message.attachments.first()
+  }
+}
+
+module.exports = File
