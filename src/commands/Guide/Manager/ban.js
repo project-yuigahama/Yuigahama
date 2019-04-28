0e2f576b3063a786a1f6ba3b7274be689c3f0862
InkoHX <inkohx@gmail.com>
2019-04-28 18:54:55 +0900

    update ban command

--- src/commands/Guide/Manager/ban.js
+++ src/commands/Guide/Manager/ban.js
@@ -1,5 +1,4 @@
 const { Command, KlasaMessage, KlasaUser } = require('klasa')
-const { GuildMember } = require('discord.js')
 
 /**
  * @extends Command
@@ -8,7 +7,7 @@ class Ban extends Command {
   constructor (...args) {
     super(...args, {
       runIn: ['text'],
-      permissionLevel: 6,
+      permissionLevel: 5,
       requiredPermissions: ['BAN_MEMBERS'],
       descripition: 'ギルドからメンバーをBANします。',
       usage: '<user:user> [days:int{1,7}] [reason:...string]',
@@ -22,13 +21,11 @@ class Ban extends Command {
    */
   async run (message, [user, days = 0, reason = 'Not specified']) {
     const member = await message.guild.members.fetch(user).catch(() => null)
-    if (message.author.id === user.id) return message.sendMessage('自分自身をBANする事は出来ません。')
-    if (this.client.user.id === user.id) return message.sendMessage('私をBANする場合このコマンドは実行出来ません。')
-    if (member instanceof GuildMember) {
+    if (!member) {
       if (member.roles.highest.position >= message.member.roles.highest.position || !member.bannable) return message.sendMessage('このユーザーはBAN出来ません。理由: 自分より上の権限を持ったユーザーかこのボットの権限ではこのユーザーをBAN出来ない為')
     }
     await message.guild.members.ban(user, { days: days, reason: reason })
-    return message.sendMessage(`${user.tag}をBANしました。<:SataniaThumbsUp:551707177249800204>`)
+    return message.sendMessage(`${user.tag}をBANしました。`)
   }
 }
 
