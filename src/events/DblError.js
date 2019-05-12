f90c1a3a2f7ae3583e5b4443934200000cbbf165
InkoHX <inkohx@gmail.com>
2019-05-12 17:51:05 +0900

    Fix DblError

--- src/events/DblError.js
+++ src/events/DblError.js
@@ -1,4 +1,5 @@
 const { Event } = require('klasa')
+const { makeErrorReport } = require('../lib/error/Reporter')
 
 /**
  * @extends Event
@@ -10,6 +11,11 @@ class DblError extends Event {
   run (error) {
     if (!(error instanceof Error)) return
     this.client.emit('error', error.stack || error)
+    if ('ERROR_REPORT_CHANNEL_ID' in process.env) {
+      this.client.channels.get(process.env.ERROR_REPORT_CHANNEL_ID).sendCode('asciidoc', makeErrorReport(error, this.client))
+        .then(() => this.client.console.log('[REPORTER] An error report has been sent.'))
+        .catch(() => this.client.console.warn('[REPORTER] Failed to send error report.'))
+    }
   }
 }
 
