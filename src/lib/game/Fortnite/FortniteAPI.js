4c48d16522a34c1324b102a48541e19588ef451a
InkoHX <inkohx@gmail.com>
2019-05-12 20:47:28 +0900

    Add FortniteAPI #14

---
+++ src/lib/game/Fortnite/FortniteAPI.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch')
+const YuigahamaError = require('../../error/YuigahamaError')
+
+class FortniteAPI {
+  static async getChallenges () {
+    const data = await fetch('https://fortnite-public-api.theapinetwork.com/prod09/challenges/get?season=current')
+      .then(res => res.json())
+      .catch(() => null)
+    if (data === null) throw new YuigahamaError('Request failed.')
+
+    return data
+  }
+
+  static async getWeapons () {
+    const data = await fetch('https://fortnite-public-api.theapinetwork.com/prod09/weapons/get')
+      .then(res => res.json())
+      .catch(() => null)
+    if (data === null) throw new YuigahamaError('Request failed.')
+
+    return data
+  }
+
+  static async getNews (lang = 'en') {
+    const data = await fetch(`https://fortnite-public-api.theapinetwork.com/prod09/br_motd/get?language=${lang}`)
+      .then(res => res.json())
+      .catch(() => null)
+    if (data === null) throw new YuigahamaError('Request failed.')
+
+    return data
+  }
+
+  static async getStatus () {
+    const data = await fetch('https://fortnite-public-api.theapinetwork.com/prod09/status/fortnite_server_status')
+      .then(res => res.json())
+      .catch(() => null)
+    if (data === null) throw new YuigahamaError('Request failed.')
+
+    return data
+  }
+}
+
+module.exports = FortniteAPI
