54d2a608800d18a5379c17470800e69c0feb61e6
InkoHX <inkohx@gmail.com>
2019-05-28 15:22:34 +0900

    Fix lint

--- src/commands/DevTools/JavaScript/mdn.js
+++ src/commands/DevTools/JavaScript/mdn.js
@@ -24,6 +24,7 @@ class MDN extends Command {
     const Display = new RichDisplay()
     const Documents = data['documents']
     if (!Documents || Documents.length === 0) return message.sendMessage(`Information matching \`${query}\` could not be found.`)
+
     Documents.forEach((value) => {
       Display.addPage(new MessageEmbed()
         .setColor('RANDOM')
