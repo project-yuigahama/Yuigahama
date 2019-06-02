e8943714dc40a4f8830d055ce706fcebbe6d2639
InkoHX <inkohx@gmail.com>
2019-06-02 16:53:24 +0900

    Add Logger class

---
+++ src/lib/util/Logger.js
@@ -0,0 +1,40 @@
+const { KlasaMessage } = require('klasa')
+const { MessageEmbed } = require('discord.js')
+
+class Logger {
+  /**
+   * @param {KlasaMessage} message
+   */
+  constructor (message) {
+    if (typeof message === 'undefined') throw new Error('Undefined message.')
+
+    /**
+     * @type {KlasaMessage}
+     */
+    this.message = message
+
+    /**
+     * @type {MessageEmbed}
+     */
+    this._embed = new MessageEmbed()
+  }
+
+  get embed () {
+    return this._embed
+  }
+
+  set embed (data) {
+    this._embed = data
+
+    return this
+  }
+
+  /**
+   * @returns {KlasaMessage|KlasaMessage[]}
+   */
+  async sendLog () {
+    this.message.sendEmbed(this.embed)
+  }
+}
+
+module.exports = Logger
