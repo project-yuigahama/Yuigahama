099bb921da91555801665bbdfe0d7fdcf72fee59
InkoHX <inkohx@gmail.com>
2019-06-03 14:58:30 +0900

    Add test

--- test/Utils.test.js
+++ test/Utils.test.js
@@ -5,17 +5,24 @@ const it = require('mocha').it
 
 describe('Utils', () => {
   describe('getPlatform', () => {
-    it('Return an string?', () => {
+    it('Return string', () => {
       assert.strictEqual(typeof utils.getPlatform(), 'string')
     })
   })
 
   describe('resolveModTypeColor', () => {
-    it('Return an integer?', () => {
+    it('Return integer', () => {
       assert.strictEqual(typeof utils.resolveModTypeColor('KICK'), 'number')
       assert.strictEqual(typeof utils.resolveModTypeColor('BAN'), 'number')
       assert.strictEqual(typeof utils.resolveModTypeColor('UNKNOWN'), 'number')
       assert.strictEqual(typeof utils.resolveModTypeColor('???'), 'number')
+    })
+
+    it('Return correct value', () => {
+      assert.strictEqual(utils.resolveModTypeColor('BAN'), 0xE74C3C)
+      assert.strictEqual(utils.resolveModTypeColor('KICK'), 0xF1C40F)
+      assert.strictEqual(utils.resolveModTypeColor('UNKNOWN'), 0x95A5A6)
+      assert.strictEqual(utils.resolveModTypeColor('???'), 0x000000)
     })
   })
 })
