dbfcb2025607e5c85c6bbf507158654d9308dee8
InkoHX <inkohx@gmail.com>
2019-06-09 17:04:02 +0900

    Fixes

--- src/languages/ja-JP.js
+++ src/languages/ja-JP.js
@@ -231,7 +231,7 @@ module.exports = class extends Language {
       COMMAND_MDN_DESCRIPTION: 'MDN Web docs を検索することができます。',
       COMMAND_TOP_INVITE_DESCRIPTION: '招待回数ランキングを表示します。',
       COMMAND_TOP_INVITE_NOT_FOUND: '招待リンクが一つもありません。',
-      IMAGE_FILTER_DELETE_MESSAGE: 'NSFWでないチャンネルにNSFW画像を送信しないでください。',
+      IMAGE_FILTER_DELETE_MESSAGE: 'NSFWチャンネルでないSFWチャンネルにNSFW画像を送信しないでください。',
       COMMAND_IMAGE_FILTER_DESCRIPTION: '強力な画像フィルター',
       COMMAND_IMAGE_FILTER_EXTENDED_HELP: '画像フィルターを使用できるのは、管理者によって承認されたサーバーだけです。',
       COMMAND_IMAGE_FILTER_ON: '画像フィルターが有効になりました。',
--- src/monitors/ImageFilter.js
+++ src/monitors/ImageFilter.js
@@ -25,33 +25,35 @@ class ImageFilter extends Monitor {
     if (attachment.url === null || attachment.width === null || attachment.height === null) return
     if (/\.(?:(?:jpe?|pn)g|tif?f|bmp)$/i.test(attachment.name) === false) return
 
-    await nsfwjs.load().then((model) => {
-      const width = attachment.width
-      const height = attachment.height
-      const image = new Image()
-      const canvas = createCanvas(width, height)
-      const ctx = canvas.getContext('2d')
-      image.src = attachment.url
-      image.onload = () => {
-        ctx.drawImage(image, 0, 0, width, height)
-        model.classify(canvas).then((predictions) => {
-          const data = predictions[0]
-          if (data.className === 'Porn' || data.className === 'Hentai') {
-            if (data.probability > 0.72 && message.deleted === false) {
-              try {
-                message.delete()
-              } catch (err) {
-                this.client.emit('error', err)
-              }
-              message.reply(message.language.get('IMAGE_FILTER_DELETE_MESSAGE'))
+    const width = attachment.width
+    const height = attachment.height
+    const image = new Image()
+    const canvas = createCanvas(width, height)
+    const ctx = canvas.getContext('2d')
+    image.src = attachment.url
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0, width, height)
+      this.model.classify(canvas).then((predictions) => {
+        const data = predictions[0]
+        if (data.className === 'Porn' || data.className === 'Hentai') {
+          if (data.probability > 0.72 && message.deleted === false) {
+            try {
+              message.delete()
+            } catch (err) {
+              this.client.emit('error', err)
             }
+            message.reply(message.language.get('IMAGE_FILTER_DELETE_MESSAGE'))
           }
-        })
-      }
-      image.onerror = () => {
-        this.client.console.warn('Processing was interrupted because the file could not be found.')
-      }
-    })
+        }
+      })
+    }
+    image.onerror = () => {
+      this.client.console.warn('Processing was interrupted because the file could not be found.')
+    }
+  }
+
+  async init () {
+    this.model = await nsfwjs.load()
   }
 }
 
