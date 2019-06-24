00e52ec901209bd2e3ca5c2c75bf82b33788b7eb
InkoHX <inkohx@gmail.com>
2019-06-24 10:29:48 +0900

    Remove messageReactionRemove.js

--- src/events/messageReactionRemove.js
+++
@@ -1,8 +0,0 @@
-const { Event, KlasaUser } = require('klasa')
-const { MessageReaction } = require('discord.js')
-
-class MessageReactionRemove extends Event {
-  async run (...params) {
-  // TODO: pushpinが削除された時の処理を書く
-  }
-}
