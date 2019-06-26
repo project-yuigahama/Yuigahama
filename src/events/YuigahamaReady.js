4d80dbbb88f0dd4252aaa2b54328072d042e05c3
InkoHX <inkohx@gmail.com>
2019-06-26 23:47:36 +0900

    Yahhalo~

--- src/events/YuigahamaReady.js
+++ src/events/YuigahamaReady.js
@@ -1,4 +1,5 @@
-const { Event } = require('klasa')
+const { Event, Colors } = require('klasa')
+const { Utils } = require('../Yui')
 
 /**
  * @extends Event
@@ -12,6 +13,14 @@ class YuigahamaReady extends Event {
 
   async run () {
     this.client.user.setActivity({ name: `yuigahama@help | ${this.client.guilds.size} guilds`, type: 'PLAYING' })
+
+    const iconArray = Utils.iconToArray()
+    const color = new Colors({ style: 'bold', text: 'magenta' })
+
+    for (let index = 0; index < iconArray.length; index++) {
+      const element = iconArray[index]
+      this.client.console.log(color.format(element))
+    }
   }
 }
 
--- src/lib/util/utils.js
+++ src/lib/util/utils.js
@@ -46,6 +46,44 @@ class Utils {
       default: return 0.6
     }
   }
+
+  /**
+   * @returns {string[]}
+   *
+   * @static
+   */
+  static iconToArray () {
+    return [
+      '',
+      '          WVTzi<<?',
+      '        EV<(C:(4e::(Ud',
+      '       #I::z:::~X<(J+WHW8WWHg',
+      '       Jl::zl::(d=!```_:::::::<7TnH',
+      '   WMMMOc:~:?jV>_.`` .:~~::~:~::::?1wH',
+      '   KHWMS6::(Z>::::::::_-_:~(_:~~~:::::(TmH',
+      '    NWez1Wg$::~+<:~:(>:~:::w::::~:~~~:~::?XU',
+      '       khZ>::~j>::(d3::~~:(V2:~::~::~::~::?4H',
+      '      #Q$J>::(D::(WD::~::_J~dc:~::~::~:<~:::wd',
+      '     R9&d3(<~dc_J3d>:~:~:j>._4/:~:~~:::I:~~:(N',
+      ' HNQkqmyf(K:(K(v7!d<::::+RZ! .4-::::~:(I::~::4d',
+      ' NgNMMMJl(P(fk+```?b:(dj% ```` W/(c~:~(>:~:::d                             Yahhalo~',
+      '     #SK<($Z`jl.`` 4x(HP```````.4J$:::j<:~(+~dd     GitHub: https://github.com/project-yuigahama/Yuigahama',
+      '     EW3:(X!.dHM!```?df!`````.`` ($:~(3:::~X+K              Patron: https://patreon.com/inkohx',
+      '   WY~J?n($ ,HyW!````` ````.jMk.`,$((+_:~::dKXd',
+      '  gX,(5(vXP(-7Y3```````````(NyW[`JK~._7s:~:?N',
+      '   NgjJ(ZZ<ZJir```````````` ?T9_.J>(h, j<:::dd',
+      '     #JmtX/_~_``````` ....``($2J(k.?&Jx0::~+_jd',
+      '     NXHytdn.``````j><~~.(< ^??:(0OUGzd$:::(UmdWd',
+      '      pkKWOtVW,.```.6_.~.~?;```.Xttttd$:_j2(KMMNNN',
+      '      mWVmTHytOVW&-..___~_-J(QWWStrtdE(JWRnXd',
+      '       NNgWmYmOttwy4dP?HD-4kOWZWOttdHqNMMdm',
+      '            NNXAtdhdMMMDd-X`4d1Kttd8',
+      '               NHHVqr.gNH?^ .h?wyOHJ',
+      '                HdjW#WM9X8   .HZH4d8WH',
+      '                @MKHD.P`?;    .Wd/RtttrVWk',
+      ''
+    ]
+  }
 }
 
 module.exports = Utils
